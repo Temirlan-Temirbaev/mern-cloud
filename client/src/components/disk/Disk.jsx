2420aa92e249fdaf49aee632f4ad289106625aa7
Temirbaev Temirlan <chinese-phones-premium@mail.ru>
2022-03-18 17:56:09 +0600

    added dirNavigation and createDir func

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,21 +1,33 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {getFiles} from "../../actions/file";
+import {setPopupDisplay, setCurrentDir} from '../../reducers/fileReducer'
+import {getFiles, createDir} from "../../actions/file";
 import FileList from "./fileList/FileList";
+import Popup from './Popup'
 import './disk.css'
 const Disk = () => {
     const dispatch = useDispatch()
     const currentDir = useSelector(state => state.files.currentDir)
+    const dirStack = useSelector(state => state.files.dirStack)
     useEffect(() => {
         dispatch(getFiles(currentDir))
     }, [currentDir])
+
+    function showPopupHandler() {
+        dispatch(setPopupDisplay('flex'))
+    }
+    function backClickHandler(){
+        const backDirId = dirStack.pop()
+        dispatch(setCurrentDir(backDirId))
+    }
     return (
         <div className="disk">
             <div className="disk__btns">
-                <button className="disk__back">Назад</button>
-                <button className="disk__create">Создать папку</button>
+                <button className="disk__back" onClick={() => backClickHandler()}>Назад</button>
+                <button className="disk__create" onClick={() => showPopupHandler()}>Создать папку</button>
             </div>
             <FileList />
+            <Popup />
         </div>
     );
 };
--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import './file.css'
 import fileLogo from '../../../../assets/img/file.svg'
 import dirLogo from '../../../../assets/img/folder.svg'
+import {useDispatch, useSelector} from "react-redux";
+import {setCurrentDir,pushToStack} from '../../../../reducers/fileReducer'
 const File = ({file}) => {
+    const dispatch = useDispatch()
+    const currentDir = useSelector(state => state.files.currentDir)
+    function openHandler() {
+        dispatch(pushToStack(currentDir))
+        dispatch(setCurrentDir(file._id))
+    }
+    function openPlayerHandler(){
+        console.log("its not dir")
+    }
     return (
-        <div className="file">
+        <div className="file" onClick={file.type ? () => openHandler() : () => openPlayerHandler()}>
             <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img" />
             <div className="file__name">{file.name}</div>
             <div className="file__date">{file.date.slice(0, 10)}</div>
